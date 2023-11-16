b387859cfbfbf16f7ba830ca2553cbfa69bc4230
mostafa.abdelhalim <mostafa.abdelhailm@b-tech.com>
2023-11-16 09:57:57 +0200

    first commit

---
+++ src/main/java/api/endpoints/Base.java
@@ -0,0 +1,37 @@
+package api.endpoints;
+
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+
+import static io.restassured.RestAssured.given;
+
+public class Base {
+    public Response postRequest(String endpoint, Object payload) {
+        Response response = given()
+                .header("API-Key","2e8f142c-4f0a-4b8a-a7b9-1234567890ab")
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+
+                .body(payload)
+                .when()
+                .post(endpoint);
+        return response;
+    }
+
+    public Response getRequest(String endpoint) {
+        Response response = given()
+                .when()
+                .get(endpoint);
+        return response;
+    }
+
+    public Response putRequest(String endpoint, Object payload) {
+        Response response = given()
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .body(payload)
+                .when()
+                .put(endpoint);
+        return response;
+    }
+}
---
+++ src/test/java/api/Tests/DDUserTest.java
@@ -0,0 +1,35 @@
+package api.Tests;
+
+import api.endpoints.Base;
+import api.endpoints.Routes;
+import io.restassured.response.Response;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+public class DDUserTest extends Base {
+    Base base;
+    String sendNotificationBody = "{\n" +
+            "  \"channel\": \"sms\",\n" +
+            "  \"priority\": 2,\n" +
+            "  \"recipient_list\": \"01594876029\",\n" +
+            "  \"template_id\": \"afafc4d9-8e19-4870-8480-d9bde6bff4bc\",\n" +
+            "  \"template_parameters\": {\n" +
+            "    \"name\":\"Mostafa\"\n" +
+            "  }\n" +
+            "}";
+
+    @BeforeClass(description = "Prepare data for testing")
+    public void setupData() throws IOException {
+        base = new Base();
+    }
+
+    @Test
+    public void sendNotification() {
+       Response response =  base.postRequest(Routes.notificationEndpoint, sendNotificationBody);
+        Assert.assertEquals(response.statusCode(),201);
+    }
+
+}
